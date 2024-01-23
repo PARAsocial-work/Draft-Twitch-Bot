34b1ab492a4d2f6661c0de8540d6f8e58f6ef8cf
sam0x2b <sam0x2b@duck.com>
2024-01-23 22:54:10 +0300

    greatly simplified nickname scramble

--- index.js
+++ index.js
@@ -92,94 +92,56 @@
 			}
 		}),
 	]});
-
+//
+//
+//
+// Scrambling nicknames
+function scramble(str) {
+    // salt
+    str += Math.random().toString(16).slice(2, 8);
+    // djb2
+    let hash = str.split('').map((s) => {
+        return s.charCodeAt(0);
+    }).reduce((e, a) => {
+        return ((e << 5) + e) + a;
+    }, 5381);
+    // need only about 3 bytes (also no negative sign)
+    hash = hash & 0xffffff;
+    let letter = String.fromCharCode(hash % 26 + 65); // 65 == 'A'
+    hash = Math.floor(hash / 26);
+	// TODO: maybe load word list from config at init stage? if so, revisit hash being ANDed with 0xffffff
+    const words = ["Ace", "Aloe", "Apple", "Aura", "Beach", "Berry", "Blank", "Blue", "Book", "Busk", "Chess", "Chip", "Clay", "Cloud", "Cocoa", "Coral", "Crow", "Dash", "Deer", "Dive", "Draw", "Easel", "East", "Etch", "Expo", "Felt", "Film", "Foam", "Font", "Gale", "Gecko", "Grain", "Grape", "Grey", "Harp", "Hazel", "Heat", "Hive", "Honey", "Hour", "Iced", "Idea", "Indie", "Jade", "Jam", "Jazz", "Kiln", "Kite", "Kiwi", "Kola", "Lane", "Leaf", "Light", "Link", "Lute", "Mail", "Mana", "Mast", "Moss", "Music", "Nano", "Nemo", "Neon", "Noon", "One", "Open", "Opal", "Orange", "Paint", "Palm", "Park", "Parka", "Petal", "Piano", "Purple", "Quay", "Quip", "Rain", "Read", "Reef", "Ring", "River", "Roll", "Sand", "Satin", "Scout", "Snow", "Spin", "Staff", "Study", "Swim", "Tale", "Toast", "Tundra", "Tune", "Uses", "Vase", "Villa", "Viola", "Wind", "Wood"];
+    let word = words[hash % words.length];
+    hash = Math.floor(hash / words.length);
+    let num = (hash % 100).toString();
+    // hash = Math.floor(hash / 100);
+    return letter + "-" + word + num
+}
 //
 //
 //
 //Handling Whispers
 	//Holder for anons that have chatted already (will make sense below)
-	var tonightsanons = [];
-
-	//Function for Messy Scramble of Usernames
-	function messy(x,y){
-		
-		//
-		//Adding salt - placeholder only
-		var x = (x + "35937289");
-		
-		//
-		//Hash it - secret placeholder only
-		var hashed = new Buffer(
-				crypto.createHmac('SHA256', '123456').update(x).digest('hex')
-			).toString('base64');
-		
-		//
-		//Remove non-alphabet characters from the hash
-		for (let i = 0; i < hashed.length; i++){
-				if (!((hashed[i] >= 'A' && hashed[i] <= 'Z') || (hashed[i] >= 'a' && hashed[i] <= 'z') || (hashed[i] >= '0' && hashed[i] <= '9'))) {
-					hashed = hashed.substring(0, i) + hashed.substring(i + 1);
-					i--;}
-		};
-		
-		//
-		//Make hash into a BigInt by replacing letters with their numerical equiv 
-		var lowercaseWord = hashed.toLowerCase();
-		var getLetterValue = letter => {
-			if (letter >= 'a' && letter <= 'z') {
-				return letter.charCodeAt(0) - 'a'.charCodeAt(0) + 1;
-			} else {
-				return 0;
-			}
-		};		
-		var values = Array.from(lowercaseWord, getLetterValue);
-		var numbervalue = String(BigInt(String(values).replace(/,/g, '')));
-		var stringnumbervaluelength = String(numbervalue).length;
-		
-		//
-		//Using the hash to create some number values for vars a, b and c 
-		var a = parseInt(numbervalue[24]) + parseInt(numbervalue[1]) + parseInt(numbervalue[75]);
-		var b = (parseInt(numbervalue[2]) + parseInt(numbervalue[9]) + parseInt(numbervalue[16]) + parseInt(numbervalue[23]) + parseInt(numbervalue[38]) + parseInt(numbervalue[43]) + parseInt(numbervalue[59]) + parseInt(numbervalue[64]) + parseInt(numbervalue[71]) + parseInt(numbervalue[80]) + parseInt(numbervalue[87]));
-		
-		var totalfours = 0;
-		for (let i = 0; i < parseInt(stringnumbervaluelength); i++) {
-			if (numbervalue[i] === '4') {
-				totalfours++;
-			}
-		};
-		
-		var c = totalfours;
-		
-		//
-		//Using a b and c to create the beginning, middle and end of an anon username
-		var randomstart = ["A-", "B-", "C-", "D-", "E-", "F-", "G-", "H-", "I-", "J-", "K-", "L-", "M-", "N-", "O-", "P-", "Q-", "R-", "S-", "T-", "U-", "V-", "W-", "X-", "Y-", "Y-", "Z-", "Z-"];
-
-		var randommiddle = ["Ace", "Aloe", "Apple", "Aura", "Beach", "Berry", "Blank", "Blue", "Book", "Busk", "Chess", "Chip", "Clay", "Cloud", "Cocoa", "Coral", "Crow", "Dash", "Deer", "Dive", "Draw", "Easel", "East", "Etch", "Expo", "Felt", "Film", "Foam", "Font", "Gale", "Gecko", "Grain", "Grape", "Grey", "Harp", "Hazel", "Heat", "Hive", "Honey", "Hour", "Iced", "Idea", "Indie", "Jade", "Jam", "Jazz", "Kiln", "Kite", "Kiwi", "Kola", "Lane", "Leaf", "Light", "Link", "Lute", "Mail", "Mana", "Mast", "Moss", "Music", "Nano", "Nemo", "Neon", "Noon", "One", "Open", "Opal", "Orange", "Paint", "Palm", "Park", "Parka", "Petal", "Piano", "Purple", "Quay", "Quip", "Rain", "Read", "Reef", "Ring", "River", "Roll", "Sand", "Satin", "Scout", "Snow", "Spin", "Staff", "Study", "Swim", "Tale", "Toast", "Tundra", "Tune", "Uses", "Vase", "Villa", "Viola", "Wind", "Wood"];
-
-		var randomend = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89"];
-
-		var userx = (randomstart[a] + randommiddle[b] + randomend[c]);
+	var tonightsanons = [];	
+	//
+	//What to do on Whisper
+	chatClient.onWhisper((user, text, message) => {
+		let userx = scramble(user)
 		
 		//Placeholder for echoing into my anon window
-		console.log("ANON MSG: " + userx + " - " + y);
+		console.log("ANON MSG: " + userx + " - " + text);
 				
 		//Before putting anon message into chat, it checks if anon has been muted
 		if (mutedanons.includes(userx) === false){
 			//First time an anon messages for the night, it adds the user to an array of tonight's anons
 			if (tonightsanons.includes(userx) != true){
 				chatClient.say("parasocial_work", ("A new anon started chatting"));
-				chatClient.say("parasocial_work", (userx + " said: " + y));
+				chatClient.say("parasocial_work", (userx + " said: " + text));
 				tonightsanons.push(userx);
 			} else {
-				chatClient.say("parasocial_work", (userx + " said: " + y));
+				chatClient.say("parasocial_work", (userx + " said: " + text));
 			}
 		}
-	};
-
-	
-	//
-	//What to do on Whisper
-	chatClient.onWhisper((user, text, message) => {
-		messy(user,text);
 	});
 
 
