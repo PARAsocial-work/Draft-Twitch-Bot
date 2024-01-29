3bdbcea4ebb7fdad2c093f536d141206999fd349
Robert Lim <robertslim42@gmail.com>
2024-01-30 00:19:37 +1100

    moves extracted functions to a new file with tests

--- index.js
+++ index.js
@@ -12,7 +12,6 @@
 	const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 	//Crypto (*booo*) for hashing (*yaaay*) 
-	import * as crypto from "crypto";
 
 	//Express for http requests
 	import express from "express";
@@ -20,6 +19,7 @@
 	import { createServer } from 'node:http';
 	import { fileURLToPath } from 'node:url';
 	import { dirname, join } from 'node:path';
+	import { generateIndexes } from './src/hash.js';
 
 //
 //
@@ -191,39 +191,13 @@
 			}
 		}
 
-const getLetterValue = letter => {
-	if (letter >= 'a' && letter <= 'z') {
-		return letter.charCodeAt(0) - 'a'.charCodeAt(0) + 1;
-	} else {
-		return 0;
-	}
-};
 	//Function for Messy Scramble of Usernames
 	function messy(x,y){
 		
 		//
 		//Adding salt - placeholder only
 		var x = (x + "35937289");
-		
-		//
-		//Hash it - secret placeholder only
-		var hashed = hash(x);
-		
-		//
-		//Remove non-alphabet characters from the hash
-		hashed = removeNonAlphabet(hashed);
-		
-		//
-		//Make hash into a BigInt by replacing letters with their numerical equiv 
-		const numbervalue = toNumberValue(hashed);
-		const stringnumbervaluelength = String(numbervalue).length;
-		
-		//
-		//Using the hash to create some number values for vars a, b and c 
-		const { a, b } = parseCryptoIndex(numbervalue);
-		
-		const c = countFours(stringnumbervaluelength, numbervalue);
-		
+		const { a, b, c } = generateIndexes(x)
 		//
 		//Using a b and c to create the beginning, middle and end of an anon username ("userx")
 		const randomstart = [...Array(26).keys()].map(i => String.fromCharCode(i + 0x41) + '-');
@@ -319,44 +293,3 @@ const getLetterValue = letter => {
 	//
 	//Connecting for Whispers
 	chatClient.connect();
-
-function countFours(stringnumbervaluelength, numbervalue) {
-	let totalfours = 0;
-	for (let i = 0; i < stringnumbervaluelength; i++) {
-		if (numbervalue[i] === '4') {
-			totalfours++;
-		}
-	};
-
-	const c = totalfours;
-	return c;
-}
-
-function parseCryptoIndex(numbervalue) {
-	const a = parseInt(numbervalue[24]) + parseInt(numbervalue[1]) + parseInt(numbervalue[75]);
-	const b = (parseInt(numbervalue[2]) + parseInt(numbervalue[9]) + parseInt(numbervalue[16]) + parseInt(numbervalue[23]) + parseInt(numbervalue[38]) + parseInt(numbervalue[43]) + parseInt(numbervalue[59]) + parseInt(numbervalue[64]) + parseInt(numbervalue[71]) + parseInt(numbervalue[80]) + parseInt(numbervalue[87]));
-	return { a, b };
-}
-
-function toNumberValue(hashed) {
-	const lowercaseWord = hashed.toLowerCase();
-	const values = Array.from(lowercaseWord, getLetterValue);
-	const numbervalue = String(BigInt(String(values).replace(/,/g, '')));
-	return numbervalue;
-}
-
-function removeNonAlphabet(hashed) {
-	for (let i = 0; i < hashed.length; i++) {
-		if (!((hashed[i] >= 'A' && hashed[i] <= 'Z') || (hashed[i] >= 'a' && hashed[i] <= 'z') || (hashed[i] >= '0' && hashed[i] <= '9'))) {
-			hashed = hashed.substring(0, i) + hashed.substring(i + 1);
-			i--;
-		}
-	};
-	return hashed;
-}
-
-function hash(x) {
-	return new Buffer(
-		crypto.createHmac('SHA256', '123456').update(x).digest('hex')
-	).toString('base64');
-}
