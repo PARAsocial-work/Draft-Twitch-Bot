51abd5cb351c18c2446314bef0e023dfde80d075
PARAsocial-work <157328509+PARAsocial-work@users.noreply.github.com>
2024-01-29 20:36:37 +1100

    Update index.js
    Updated with latest version of index.js - 
    
    - added express + socket for the client browser functionality
    - amended pronoun approach per alejo's request
    - added automated message from bot into chat 
    - amended names so that anonymous whispers are given unique word 
    - added breakdown of long whispers to support message size 
    - mild amount of tidying, annotation and better practice happened
    
    Still needs to be split up and tidying could use more work.

--- index.js
+++ index.js
@@ -9,10 +9,34 @@
 
 	//Needed FS to effectively fetch pronouns
 	import { promises as fs } from 'fs';
-	var fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+	const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 	//Crypto (*booo*) for hashing (*yaaay*) 
 	import * as crypto from "crypto";
+
+	//Express for http requests
+	import express from "express";
+	import { Server } from 'socket.io';
+	import { createServer } from 'node:http';
+	import { fileURLToPath } from 'node:url';
+	import { dirname, join } from 'node:path';
+
+//
+//
+//
+//Opening App + Socket to pass information to the browser page 
+	const __dirname = dirname(fileURLToPath(import.meta.url));
+	const app = express();
+	const server = createServer(app);
+	const io = new Server(server);
+
+	app.get('/', (req, res) => {
+		res.sendFile(join(__dirname, '/client/browser.html'));
+	});
+
+	server.listen(3000, () =>
+		console.log("We're golden")
+	);
 
 //
 //
@@ -20,10 +44,10 @@
 //Connecting Bot
 
 	//Authorisation Token + Refresher
-	var clientId = 'nicetry000000';
-	var clientSecret = 'nicetry000000';
-	var tokenData = JSON.parse(await fs.readFile('./tokens.xyz.json', 'UTF-8'));
-	var authProvider = new RefreshingAuthProvider(
+	const clientId = 'nicetry000000';
+	const clientSecret = 'nicetry000000';
+	const tokenData = JSON.parse(await fs.readFile('./tokens.xyz.json', 'UTF-8'));
+	const authProvider = new RefreshingAuthProvider(
 		{
 			clientId,
 			clientSecret
@@ -33,51 +57,99 @@
 	await authProvider.addUserForToken(tokenData, ['chat']);
 
 	//Connecting to Chat
-	var chatClient = new ChatClient({ authProvider, channels: ['parasocial_work'] });
+	const chatClient = new ChatClient({ authProvider, channels: ['parasocial_work'] });
 
+//
+//
+//
+//Automated Message Explaining Bot Functionality 
+
+	//Announces on entry
+	chatClient.onConnect(() => {
+		chatClient.say("parasocial_work", ("If you want to message (quasi-)anonymously, whisper me: the PSW_anons bot. Please check the 'Anon Messages' section below for details."));
+		//Then announces every 20 minutes while connected
+		const twentymins = (20*(60 * 1000));
+		setInterval(function() {
+    		chatClient.say("parasocial_work", ("If you want to message (quasi-)anonymously, whisper me: the PSW_anons bot. Please check the 'Anon Messages' section below for details."));
+		}, twentymins);
+		//Browser Socket Connection Confirmation 
+		io.emit("conn", "Connected");
+			//Refreshes connection indicator every 5 mins
+			setInterval(function() {
+				console.log(`We're STILL golden`);
+				io.emit("conn", "Connected");
+			}, 100000)
+	});
 
 //
 //
 //
-//Reading Chat
+//Pronouns + Reading Chat
+//(Note - I checked with Alejo via his discord and confirmed consent to do this, and he requested pronouns are saved for ~5 minute period to minimize requests)
 
 	//Possible Pronouns
-	var pronounlist = [{"name":"aeaer","display":"Ae/Aer"},{"name":"any","display":"Any"},{"name":"eem","display":"E/Em"},{"name":"faefaer","display":"Fae/Faer"},{"name":"hehim","display":"He/Him"},{"name":"heshe","display":"He/She"},{"name":"hethem","display":"He/They"},{"name":"itits","display":"It/Its"},{"name":"other","display":"Other"},{"name":"perper","display":"Per/Per"},{"name":"sheher","display":"She/Her"},{"name":"shethem","display":"She/They"},{"name":"theythem","display":"They/Them"},{"name":"vever","display":"Ve/Ver"},{"name":"xexem","display":"Xe/Xem"},{"name":"ziehir","display":"Zie/Hir"}];
+	const pronounlist = [{"name":"aeaer","display":"Ae/Aer"},{"name":"any","display":"Any"},{"name":"eem","display":"E/Em"},{"name":"faefaer","display":"Fae/Faer"},{"name":"hehim","display":"He/Him"},{"name":"heshe","display":"He/She"},{"name":"hethem","display":"He/They"},{"name":"itits","display":"It/Its"},{"name":"other","display":"Other"},{"name":"perper","display":"Per/Per"},{"name":"sheher","display":"She/Her"},{"name":"shethem","display":"She/They"},{"name":"theythem","display":"They/Them"},{"name":"vever","display":"Ve/Ver"},{"name":"xexem","display":"Xe/Xem"},{"name":"ziehir","display":"Zie/Hir"}];
 
+	//Storing Pronouns for 5 mins before removing them
+	const savedUserPronouns = new Map();
 
 	//Listening for messages in chat
 	chatClient.onMessage((channel, user, message) => {
-
-		//Doing a pronoun check to add pronouns to the message (Note - I checked with Alejo via his discord and confirmed consent to do this)	
+		const x = user;
+		//Check if we've previously saved their pronouns locally
+		if (savedUserPronouns.has(x)) {
+			var userpronouns = savedUserPronouns.get(x).display;	
+			
+			//Actually sending the chat message into the Browser Source window
+           		var finalmessage = [[user], [userpronouns], [message]];
+			io.emit("chatter", finalmessage);
+			
+			return
+			
+		} else {
+		//Doing a pronoun check to add pronouns to the message 
 		fetch("https://pronouns.alejo.io/api/users/" + user)
 			.then(response => response.json())
 			.then(json => {
 				// If pronouns assigned
 				if (JSON.parse(JSON.stringify(json))[0] != undefined){
-					var pronounraw = JSON.parse(JSON.stringify(json))[0].pronoun_id;
-						for (var i = 0; i < pronounlist.length; i++){
-							if (pronounlist[i].name === pronounraw){
-								var userpronouns = pronounlist[i].display;
-							}
+					const pronounraw = JSON.parse(JSON.stringify(json))[0].pronoun_id;
+					for (let i = 0; i < pronounlist.length; i++){
+                        		//If they've assigned pronouns, store them locally for a while to reduce traffic to alejo's website
+						if (pronounlist[i].name === pronounraw){
+							var userpronouns = pronounlist[i].display;
+							savedUserPronouns.set(x, {"pronouns":pronounraw,"display":pronounList[i].display});
+			                        	//Expiring local storage of their pronoun after 5 minutes
+			                        	setTimeout(() => {
+								savedUserPronouns.delete(x)
+							}, 300000);
 						}
-					//Placeholder for echoing into my chat window
-					console.log(`${user} (${userpronouns}) : ${message}`);
+					}
 				} 
 				// If no pronouns assigned
 				else {
-					//Placeholder for echoing into my chat window
-					console.log(`${user} : ${message}`);
-				};
+				// If the pronouns aren't set, set them as "Nil" (won't show up) for the next 2.5 minutes
+					savedUserPronouns.set(x, {"pronouns":"Nil","display":"Nil"});
+		                	setTimeout(() => {
+		                        	savedUserPronouns.delete(x);
+		                    	}, 150000);
+				}
+				
+				//Actually sending the chat message into the Browser Source window
+				var finalmessage = [[user], [userpronouns], [message]];
+				io.emit("chatter", finalmessage);
 			});	
+		}
 	});
 
+//
 //
 //
 // Muting Anons
 // thought it'd be prudent to give mods the power to mute an anon
 	
-	var mutedanons = [];
-	var modsList = ["parasocial_work", "mod1", "mod2", "mod3"]; //Turns out Twitch expects broadcaster perms to ask for a modlist? smh
+	const mutedanons = [];
+	const modsList = ["parasocial_work", "mod1", "mod2", "mod3"]; //Turns out Twitch expects broadcaster perms to ask for a modlist? smh
 	
 	const bot = new Bot({ authProvider, channels: ["parasocial_work"], commands: [
 		// !muteanon Command
@@ -87,6 +159,8 @@
 			if (modsList.includes(userName) === true){
 				mutedanons.push(String(params));
 				say(String(params) + " has been muted from anon posting for the day");
+		                var finalmessage = [["SYSTEM"], [String(userx) + " has been muted from anon posting for the day"]];
+		                io.emit("anons", finalmessage);
 			} else {
 				say ("Mod only command")
 			}
@@ -96,9 +170,26 @@
 //
 //
 //
+//Checking If Caught In Automod Function
+	//PLACEHOLDER 
+
+//
+//
+//
 //Handling Whispers
 	//Holder for anons that have chatted already (will make sense below)
-	var tonightsanons = [];
+	const tonightsanons = new Map();
+
+	//Holder for anons that trigger a spam warning
+	const spamwarning = new Map();
+
+		//Function to check how many times they've spammed
+		function getByValue(map, searchValue) {
+			for (let [key, value] of map.entries()) {
+				if (key === searchValue)
+				return value;
+			}
+		}
 
 	//Function for Messy Scramble of Usernames
 	function messy(x,y){
@@ -123,56 +214,115 @@
 		
 		//
 		//Make hash into a BigInt by replacing letters with their numerical equiv 
-		var lowercaseWord = hashed.toLowerCase();
-		var getLetterValue = letter => {
+		const lowercaseWord = hashed.toLowerCase();
+		const getLetterValue = letter => {
 			if (letter >= 'a' && letter <= 'z') {
 				return letter.charCodeAt(0) - 'a'.charCodeAt(0) + 1;
 			} else {
 				return 0;
 			}
 		};		
-		var values = Array.from(lowercaseWord, getLetterValue);
-		var numbervalue = String(BigInt(String(values).replace(/,/g, '')));
-		var stringnumbervaluelength = String(numbervalue).length;
+		const values = Array.from(lowercaseWord, getLetterValue);
+		const numbervalue = String(BigInt(String(values).replace(/,/g, '')));
+		const stringnumbervaluelength = String(numbervalue).length;
 		
 		//
 		//Using the hash to create some number values for vars a, b and c 
-		var a = parseInt(numbervalue[24]) + parseInt(numbervalue[1]) + parseInt(numbervalue[75]);
-		var b = (parseInt(numbervalue[2]) + parseInt(numbervalue[9]) + parseInt(numbervalue[16]) + parseInt(numbervalue[23]) + parseInt(numbervalue[38]) + parseInt(numbervalue[43]) + parseInt(numbervalue[59]) + parseInt(numbervalue[64]) + parseInt(numbervalue[71]) + parseInt(numbervalue[80]) + parseInt(numbervalue[87]));
+		const a = parseInt(numbervalue[24]) + parseInt(numbervalue[1]) + parseInt(numbervalue[75]);
+		const b = (parseInt(numbervalue[2]) + parseInt(numbervalue[9]) + parseInt(numbervalue[16]) + parseInt(numbervalue[23]) + parseInt(numbervalue[38]) + parseInt(numbervalue[43]) + parseInt(numbervalue[59]) + parseInt(numbervalue[64]) + parseInt(numbervalue[71]) + parseInt(numbervalue[80]) + parseInt(numbervalue[87]));
 		
-		var totalfours = 0;
+		let totalfours = 0;
 		for (let i = 0; i < parseInt(stringnumbervaluelength); i++) {
 			if (numbervalue[i] === '4') {
 				totalfours++;
 			}
 		};
 		
-		var c = totalfours;
+		const c = totalfours;
 		
 		//
-		//Using a b and c to create the beginning, middle and end of an anon username
-		var randomstart = ["A-", "B-", "C-", "D-", "E-", "F-", "G-", "H-", "I-", "J-", "K-", "L-", "M-", "N-", "O-", "P-", "Q-", "R-", "S-", "T-", "U-", "V-", "W-", "X-", "Y-", "Y-", "Z-", "Z-"];
-
-		var randommiddle = ["Ace", "Aloe", "Apple", "Aura", "Beach", "Berry", "Blank", "Blue", "Book", "Busk", "Chess", "Chip", "Clay", "Cloud", "Cocoa", "Coral", "Crow", "Dash", "Deer", "Dive", "Draw", "Easel", "East", "Etch", "Expo", "Felt", "Film", "Foam", "Font", "Gale", "Gecko", "Grain", "Grape", "Grey", "Harp", "Hazel", "Heat", "Hive", "Honey", "Hour", "Iced", "Idea", "Indie", "Jade", "Jam", "Jazz", "Kiln", "Kite", "Kiwi", "Kola", "Lane", "Leaf", "Light", "Link", "Lute", "Mail", "Mana", "Mast", "Moss", "Music", "Nano", "Nemo", "Neon", "Noon", "One", "Open", "Opal", "Orange", "Paint", "Palm", "Park", "Parka", "Petal", "Piano", "Purple", "Quay", "Quip", "Rain", "Read", "Reef", "Ring", "River", "Roll", "Sand", "Satin", "Scout", "Snow", "Spin", "Staff", "Study", "Swim", "Tale", "Toast", "Tundra", "Tune", "Uses", "Vase", "Villa", "Viola", "Wind", "Wood"];
-
-		var randomend = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89"];
-
-		var userx = (randomstart[a] + randommiddle[b] + randomend[c]);
+		//Using a b and c to create the beginning, middle and end of an anon username ("userx")
+		const randomstart = [...Array(26).keys()].map(i => String.fromCharCode(i + 0x41) + '-');
+		var randommiddle = ["Grain", "Mail", "Blank", "Staff", "Chess", "Lute", "Draw", "Kola", "Ace", "Mana", "Neon", "River", "Tundra", "Dash", "Bronze", "Wood", "Zebra", "Teal", "Grape", "Film", "Honey", "Light", "Roll", "Gale", "Jade", "Beach", "Abode", "Gecko", "Wind", "Nano", "Orange", "Easel", "Yellow", "Foam", "Cabin", "Idea", "Deer", "Paint", "Tale", "Druid", "One", "Music", "Apple", "Rally", "Expo", "Swim", "Grey", "Lane", "Study", "Vase", "Delta", "Busk", "Here", "Cloud", "Sheer", "Kiwi", "Orange", "Toast", "Spirit", "Notes", "Cocoa", "Leaf", "Cloud", "Petal", "Frost", "Cotton", "Quip", "Coral", "Inn", "Piano", "Wand", "Kite", "Trunk", "Reef", "Colour", "Purple", "Lion", "Sand", "Pear", "Hour", "Tau", "Link", "Mast", "Green", "Hazel", "Satin", "Kiln", "Dew", "Gold", "Month", "East", "Palm", "Felt", "Jazz", "Read", "Moss", "Hive", "Lake", "Jam", "Cast", "Viola", "Tune", "Crow", "Clay", "Grey", "Berry", "Mist", "Aloe", "Gel", "Noon", "Scout", "Ring", "Fall", "Autumn", "Iced", "Harp", "Etch", "Blue", "North", "Jam", "Year", "Chip", "Spin", "Rain", "Park", "Uses", "Heat", "Font", "Silver", "Dive", "Book", "Aura", "Snow", "Parka", "Villa", "Open", "Nemo", "Indie", "Opal", "Air", "Red", "Shore", "Ink", "Nest", "Crow", "Oasis", "Art", "Home", "Coat", "Peak", "West", "East", "Vine", "South", "Olive", "June", "Yarn", "Shire", "Tale", "Hold", "Skies", "Cats", "Dogs", "Mage", "Card", "White", "Right", "Found", "Iris", "Bass", "Monday", "Centre"];
+		const userx = (randomstart[a] + randommiddle[b] + c);
 		
-		//Placeholder for echoing into my anon window
-		console.log("ANON MSG: " + userx + " - " + y);
-				
-		//Before putting anon message into chat, it checks if anon has been muted
-		if (mutedanons.includes(userx) === false){
-			//First time an anon messages for the night, it adds the user to an array of tonight's anons
-			if (tonightsanons.includes(userx) != true){
-				chatClient.say("parasocial_work", ("A new anon started chatting"));
-				chatClient.say("parasocial_work", (userx + " said: " + y));
-				tonightsanons.push(userx);
-			} else {
-				chatClient.say("parasocial_work", (userx + " said: " + y));
+		//Function to remove the word used at b from future anon name options
+		function removeUsernameOption(){
+			var option = randommiddle.indexOf(randommiddle[b]);
+			if (option !== -1) {
+				randommiddle.splice(option, 1);
 			}
+		}	
+		//Before putting anon message into chat, it checks if anon has been muted
+		if (mutedanons.includes(userx)){
+			return;
 		}
+		//First time an anon messages for the night, it adds the user to tonight's anon list
+		if (tonightsanons.has(x) != true){
+			chatClient.say("parasocial_work", ("A new anon started chatting"));
+			tonightsanons.set(x, userx);
+			removeUsernameOption();
+            		var finalmessage = [["SYSTEM"], ["A new anon has started chatting"]];
+			io.emit("anons", finalmessage);
+		} 
+		
+		//Checking size of message and, if over 240 characters,  breaking it down into multiple messages of 200 chars each and sending to the chat 
+        	if (String(userx + ": " + y).length > 240) {
+			
+            		var brokenmessage = y.match(/.{1,200}(?:\s|$)/g);
+
+			//Adding spam warning for messages over 800 chars long 
+			if (brokenmessage.length > 4) {
+				
+				//Giving users 3 chances with spam - 
+				if (getByValue(spamwarning, x) === undefined) {
+					spamwarning.set(x, {val: 1});
+					var warningno = "1 of 3.";
+				} else if (getByValue(spamwarning, x).val === 1) {
+					spamwarning.get(x).val++;
+					var warningno = "2 of 3.";
+				} else if (getByValue(spamwarning, x).val === 2) {
+					spamwarning.get(x).val++;
+					var warningno = "3 of 3.";
+				} else if (getByValue(spamwarning, x).val === 3) {
+					mutedanons.push(String(userx));
+					chatClient.say("parasocial_work", (String(userx) + " has been muted from anon posting for the day"));
+					var finalmessage = [["SYSTEM"], [String(userx) + " has been muted from anon posting for the day"]];
+					io.emit("anons", finalmessage);
+					return;
+				}
+				
+				chatClient.say("parasocial_work", ("Anon, the msg recieved was over 800 characters - please don't spam the chat. Spam warning " + warningno));
+				var finalmessage = [["SYSTEM"], ["Anon, the msg recieved was over 800 characters - please don't spam the chat. Spam warning " + warningno]];
+				io.emit("anons", finalmessage);
+				return;
+				
+			} else {
+
+				chatClient.onMessageFailed((channel, reason) =>{
+					chatClient.say("parasocial_work", ("Message Error"));
+				})
+				
+				//If not spam, iterating through the message's parts and sending them to the chat  
+				for (let i = 0; i < brokenmessage.length; i++){
+					const partno = i + 1;
+					const totalno = brokenmessage.length;
+					let message = (userx + " - "  + "(msg " + partno + " of " + totalno + "): " + brokenmessage[i]);
+					//PLACEHOLDER - currently debugging - console.log(checkautomod(message));
+					chatClient.say("parasocial_work", message);
+					var finalmessage = [[userx], ["(msg " + partno + " of " + totalno + "): " + brokenmessage[i]]];
+					io.emit("anons", finalmessage);
+				};
+			}
+			
+        	} else {
+			//It's just a short message, send it
+			let message = (userx + ": " + y);
+			//PLACEHOLDER - currently debugging - console.log(checkautomod(message));
+            		chatClient.say("parasocial_work", message);
+			var finalmessage = [[userx], [y]];
+			io.emit("anons", finalmessage);
+        	}
 	};
 
 	
